38cd39408c30b3bd76c35b670c5b344e5cada4ea
Erik Schwartz <erik@tablexi.com>
2013-06-01 20:21:02 -0500

    Add responsive slider

---
+++ js/main.js
@@ -0,0 +1,8 @@
+$(function() {
+  $(".rslides").responsiveSlides({
+    auto: true,             // Boolean: Animate automatically, true or false
+    speed: 500,            // Integer: Speed of the transition, in milliseconds
+    timeout: 4000,          // Integer: Time between slide transitions, in milliseconds
+    pager: false
+  });
+});
